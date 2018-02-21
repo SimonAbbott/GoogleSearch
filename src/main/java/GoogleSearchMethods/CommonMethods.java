e8f4c22d1dd4313fce3cf592cbcff694c75a1b8c
Si <abbo316@msn.com>
2018-02-21 21:22:20 +0000

    Simple google search test is now passing.

--- src/main/java/GoogleSearchMethods/CommonMethods.java
+++ src/main/java/GoogleSearchMethods/CommonMethods.java
@@ -9,20 +9,31 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonMethods {
-	WebDriver browser;
-	WebDriverWait wait;
+	public WebDriver browser;
+	public WebDriverWait wait;
+	
+	public String searchTerm;
 	
 	public void launchBrowser() {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/WebDrivers/chromedriver.exe");
 		browser = new ChromeDriver();
 		browser.navigate().to("http://www.google.co.uk");
+		
+		wait = new WebDriverWait(this.browser, 5);
 	}
 	
-	public void googleSearchForTerm(String searchString) {
-		wait = new WebDriverWait(this.browser, 5);
+	public void googleSearchForTerm() {
 		WebElement searchInput = this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[contains(@title, 'Search')]")));
-		searchInput.sendKeys(searchString);
+		searchInput.sendKeys(searchTerm);
 		searchInput.sendKeys(Keys.ENTER);
+	}
+	
+	public WebElement getSearchResults() {
+		return this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@data-async-context, 'query:" + searchTerm + "')]")));
+	}
+	
+	public Boolean isExpectedValueInResults(String expectedValue) {
+		return getSearchResults().findElements(By.xpath("//*[contains(text(), '" + expectedValue +"')]")).size() > 0 ? true: false;
 	}
 	
 	public void closeBrowser() {
--- src/test/java/GoogleSearch/StepDefinitions.java
+++ src/test/java/GoogleSearch/StepDefinitions.java
@@ -1,5 +1,7 @@
 package GoogleSearch;
 
+import static org.junit.Assert.assertTrue;
+
 import GoogleSearchMethods.CommonMethods;
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
@@ -8,7 +10,7 @@ import cucumber.api.java.en.When;
 
 public class StepDefinitions {
 	
-	CommonMethods commonMethods = new CommonMethods();
+	public CommonMethods commonMethods = new CommonMethods();
 	
 	@Given ("^I navigate to google.co.uk in the Google Chrome browser$")
 	public void iNavigateToTheWebsiteInTheBrowser() {
@@ -16,12 +18,14 @@ public class StepDefinitions {
 	}
 	
 	@When ("^I Google search for the term (.*)$")
-	public void iGoogleSearchForTheTerm(String searchString) {
-		commonMethods.googleSearchForTerm(searchString);
+	public void iGoogleSearchForTheTerm(String searchTerm) {
+		commonMethods.searchTerm = searchTerm;
+		commonMethods.googleSearchForTerm();
     }
 	
-	@Then ("^The website https://en.wikipedia.org/wiki/Software_testing is displayed in the search results$")
-	public void theWebsiteIsDisplayedInTheSeachResults() {
+	@Then ("^The website (.*) is displayed in the search results$")
+	public void theWebsiteIsDisplayedInTheSeachResults(String expectedValue) {
+		assertTrue(commonMethods.isExpectedValueInResults(expectedValue));
 	}
 	
 	@After()
